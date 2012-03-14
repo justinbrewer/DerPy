d8633f2e2e5385350e1888d52d5b5e81df78d233
Justin Brewer <madoushi90@gmail.com>
2012-03-14 03:17:43 -0500

    Thread-Safety for Database Module
    MySOL is not thread-safe per-connection, i.e no two threads can share the same
    connection. Now, if to use the current Database Engine, instead of calling
    DB.Execute, it should be DB().Execute.

--- core/Database.py
+++ core/Database.py
@@ -1,22 +1,6 @@
 import MySQLdb
-import re
 
 from Error import HTTPException
-
-DB = None
-
-def Connect():
-    cfg_file = open('CONFIG','r')
-    dsn = cfg_file.readline().strip()
-    cfg_file.close()
-    
-    conn_info = re.match(r"^(\w+)\:(?:user=(\w+)(?:;|$)|password=(\w+)(?:;|$)|host=([\w\.\-]+)(?:;|$)|dbname=([\w_]+)(?:;|$))+",dsn)
-    proto,user,password,host,dbname = conn_info.groups()
-    
-    if proto == 'mysql':
-        DB = MySQLEngine(host,user,password,dbname)
-    else:
-        raise HTTPException(500)
 
 class _DatabaseEngine:
     def __init__(self):
@@ -27,3 +11,27 @@ class MySQLEngine(_DatabaseEngine):
     def __init__(self,host,user,passwd,db):
         self.conn = MySQLdb.connect(host=host,user=user,passwd=passwd,db=db)
         self.cursor = self.conn.cursor()
+
+#--------------------
+import re
+__cfg_file = open('CONFIG','r')
+__dsn = __cfg_file.readline().strip()
+__cfg_file.close()
+__conn_info = re.match(r"^(\w+)\:(?:user=(\w+)(?:;|$)|password=(\w+)(?:;|$)|host=([\w\.\-]+)(?:;|$)|dbname=([\w_]+)(?:;|$))+",__dsn)
+
+_conn_info = list(__conn_info.groups())
+
+del [__conn_info,__dsn,__cfg_file]
+
+#--------------------
+import threading
+_store = threading.local()
+
+def Connect():
+    if _conn_info[0] == 'mysql':
+        _store.dbe = MySQLEngine(_conn_info[3],_conn_info[1],_conn_info[2],_conn_info[4])
+    else:
+        raise HTTPException(500)
+
+def DB():
+    return _store.dbe
